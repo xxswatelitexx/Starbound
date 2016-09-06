a95fb6ceb32b10dda5ee6fad4b74540706c48d20
Mr. Kal <xpiratosx@gmail.com>
2016-09-06 19:34:15 -0400

    Experiment 1
    Removed OMC - as C0bra has taken over the project.
    Addd Experiment one data storage.

---
+++ mad_Exp/data_storage/data_storage_01/btest.lua
@@ -0,0 +1,23 @@
+function init()
+  object.setInteractive(true)
+end
+
+function update()
+  test = config.getParameter("test", "bca")
+  sb.logInfo(test)
+  --Update is constantly called, so since I need to check if the value is being written into the object. It is kept here.
+  --config.getParametery(x, y) works by -  x = the value to look for in the JSON. y = if x does not exist, replace it with so and so value.
+  --With that said if the object has never been interacted with, it will show "bca". After interaction the value will always show abc.
+end
+
+function onInteraction(args)
+  sb.logInfo ("Has been interacted with")
+  object.setConfigParameter("test", "abc")
+  --object.setConfigParameter(x,y) x = JSON value, y = value to add or replace existing value with.
+  --So when the object is interacted the new data will show abc in the log.
+end
+
+function uninit()
+  sb.logInfo("Has been removed")
+  -- This snippet is just a simply point breaker in time in the log to know when the object was removed. So when replaced we know if the value has changed.
+end
---
+++ mad_Exp/data_storage/readme.md
@@ -0,0 +1,9 @@
+In this set of experiments we will look at how to store data in an object and retain it on destruction.
+A lot of mods have been doing this such has Improved Containers or Enhanced storage so I wanted to see how.
+
+#Experiment 1
+This experiment was made possible to v6ooo without him. I don't think the information provided here would have ever been known.
+In this test we use a parameter which was added in back in 2014 which was little known,
+"retainObjectParametersInItem" : true,
+
+By adding this value into the JSON file I was able to retain the JSON stored data even after destruction as the experiment will show.
